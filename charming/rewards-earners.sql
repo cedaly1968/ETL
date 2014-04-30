aba90b614159e5d7f20b37d2791dc55efce6937a
twall <tawall@dashfacts.com>
2014-04-30 18:42:43 -0400

    stupid negative numbers

--- charming/rewards-earners.sql
+++ charming/rewards-earners.sql
@@ -1,6 +1,6 @@
 use charming;
 drop table if exists rewards_earners;
 create table rewards_earners(customer_id int(10) unsigned,
- 							 field2 int(10) unsigned,
- 							 field3 int(10) unsigned,
+ 							 field2 int(10),
+ 							 field3 int(10),
  							 field4 decimal(12,6)) engine = MyISAM;
