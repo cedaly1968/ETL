cac5d78323b717b09de6ec73dbcb33cef08a9243
twall <tawall@dashfacts.com>
2014-04-29 21:56:15 -0400

    Adding reporting-details ktr and sql for charming charlie work

---
+++ charming/reporting-details.sql
@@ -0,0 +1,18 @@
+use charming;
+drop table if exists reporting_details;
+create table reporting_details(transaction_id int(10) unsigned,
+	                           transaction_line_no int(10) unsigned,
+	                           sale_or_return_indicator	char(1),
+	                           sales_associate_no int(10) unsigned,
+	                           style_id	int(10) unsigned,
+	                           color_code smallint(5) unsigned,
+	                           size_description	varchar(32),
+	                           quantity	smallint(5) unsigned,
+	                           net_retail decimal(7,2),
+	                           cost	decimal(7,2),
+	                           markdown_percent	smallint(5) unsigned,
+	                           coupon_code varchar(32),
+	                           comments	text,
+	                           promotion_flag smallint(5) unsigned,
+	                           class_code int(10) unsigned,
+	                           net_retail_central decimal(7,2)) engine=MyISAM;
